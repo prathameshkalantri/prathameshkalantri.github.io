35f19f55d77cef353971430714c60f0e742c4725
prathameshkalantri <kalantriprathamesh@gmail.com>
2024-08-18 23:47:52 -0700

    update Experience

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e7ecac4f31774021df053a500e02fcdd.js"
+  "./precache-manifest.58783ce9550148b8ff37c0255c55d6cb.js"
 );
 
 self.addEventListener('message', (event) => {
